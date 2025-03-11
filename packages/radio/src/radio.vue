b494176f73873c57a0c4551c5e8b57a5fd548e3e
wangzhilong <wangzhilongh@163.com>
2025-03-11 14:10:10 +0800

    fix: Remove aria-hidden on a focused element

--- packages/radio/src/radio.vue
+++ packages/radio/src/radio.vue
@@ -26,7 +26,6 @@
         class="el-radio__original"
         :value="label"
         type="radio"
-        aria-hidden="true"
         v-model="model"
         @focus="focus = true"
         @blur="focus = false"
